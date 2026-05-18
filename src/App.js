5122643ba141bddb8fde0423e59daa5f8633ae4e
NetNex Global <120565907+NETNEX@users.noreply.github.com>
2026-05-18 15:52:34 +0530

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import EventCheckInApp from './EventCheckInApp';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <EventCheckInApp />
+    </div>
+  );
+}
+
+export default App;
